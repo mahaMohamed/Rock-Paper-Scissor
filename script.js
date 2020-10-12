cd782a070ddb473d6e5709dbc9f2d43748733dba
mahaMohamed <maha.fagal@gmail.com>
2020-10-12 16:44:18 +0200

    Edited Indentations

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /*Initializing Variables*/
 const buttons = document.querySelectorAll("button");
 let playerSelection;
@@ -14,81 +15,86 @@ const computerScore = document.querySelector("#computer-score");
 
 /*Adding Event Listeners*/
 for (let i = 0; i < buttons.length; i++)
-  buttons[i].addEventListener("click", playGame);
+    buttons[i].addEventListener("click", playGame);
 
-
-
-
-  
 /*Contains the game logic*/
 function playGame(e) {
-  playerSelection = e.target.textContent;
-  let computerSelection = computerPlay();
-  playerMove.innerText = "Player's Move: " + playerSelection;
-  computerMove.innerText = "Computer's Move: " + computerSelection;
-  roundResult = playRound(computerSelection, playerSelection);
-  roundResultDisplay.innerText = roundResult;
 
-  if (roundResult.includes("win")) {
-    playerWins++;
-  } else if (roundResult.includes("Tie")) {
-    playerWins++;
-    computerWins++;
-  } else {
-    computerWins++;
-  }
+    playerSelection = capitalizeFirstLetter(e.target.id);
 
-  playerScore.innerText = "Your Score : " + playerWins;
-  computerScore.innerText = "Computer's Score : " + computerWins;
+    let computerSelection = computerPlay();
 
-  /*Game is terminated once a player scores 5 points*/
-  if (computerWins == 5 && playerWins <= 5) {
-    playerWins < 5
-      ? (finalResult.innerText = "You lost!")
-      : (finalResult.innerText = "Tie");
+    playerMove.innerText = "Player's Move: " + playerSelection;
 
-    for (let i = 0; i < buttons.length; i++)
-      buttons[i].removeEventListener("click", playGame);
-  }
+    computerMove.innerText = "Computer's Move: " + computerSelection;
 
-  if (playerWins == 5 && computerWins <= 5) {
-    computerWins < 5
-      ? (finalResult.innerText = "You won!")
-      : (finalResult.innerText = "Tie");
+    roundResult = playRound(computerSelection, playerSelection);
 
-    for (let i = 0; i < buttons.length; i++)
-      buttons[i].removeEventListener("click", playGame);
-  }
+    roundResultDisplay.innerText = roundResult;
+
+    if (roundResult.includes("win")) {
+        playerWins++;
+    } else if (roundResult.includes("Tie")) {
+        playerWins++;
+        computerWins++;
+    } else {
+        computerWins++;
+    }
+
+    playerScore.innerText = "Your Score : " + playerWins;
+    computerScore.innerText = "Computer's Score : " + computerWins;
+
+    
+
+    /*Game is terminated once a player scores 5 points*/
+    if (computerWins == 5 && playerWins <= 5) {
+        playerWins < 5
+            ? (finalResult.innerText = "You lost!")
+            : (finalResult.innerText = "Tie");
+
+        for (let i = 0; i < buttons.length; i++)
+            buttons[i].removeEventListener("click", playGame);
+    }
+
+    if (playerWins == 5 && computerWins <= 5) {
+        computerWins < 5
+            ? (finalResult.innerText = "You won!")
+            : (finalResult.innerText = "Tie");
+
+        for (let i = 0; i < buttons.length; i++)
+            buttons[i].removeEventListener("click", playGame);
+    }
 }
+
 
 /*Computes a random unmber to represent the Computer's Move*/
 function computerPlay() {
-  let randomNumber = Math.floor(Math.random() * 3);
+    let randomNumber = Math.floor(Math.random() * 3);
 
-  let computerSelection =
-    randomNumber == 0 ? "Rock" : randomNumber == 1 ? "Paper" : "Scissor";
+    let computerSelection =
+        randomNumber == 0 ? "Rock" : randomNumber == 1 ? "Paper" : "Scissor";
 
-  return computerSelection;
+    return computerSelection;
 }
 
 /*Computes the winner based on the game's rules*/
 function playRound(computerSelection, playerSelection) {
-  if (computerSelection == playerSelection) return "Tie";
-  else if (computerSelection == "Rock" && playerSelection == "Paper")
-    return "You win! Paper beats Rock";
-  else if (computerSelection == "Paper" && playerSelection == "Scissor")
-    return "You win! Scissors beat paper";
-  else if (computerSelection == "Scissor" && playerSelection == "Rock")
-    return "You win! Rock beats Scissors";
-  else if (computerSelection == "Scissor" && playerSelection == "Paper")
-    return "You lose! Scissors beat Paper";
-  else if (computerSelection == "Paper" && playerSelection == "Rock")
-    return "You lose! Paper beats Rock";
-  else if (computerSelection == "Rock" && playerSelection == "Scissor")
-    return "You Lose! Rock beats Scissors";
+    if (computerSelection == playerSelection) return "Tie";
+    else if (computerSelection == "Rock" && playerSelection == "Paper")
+        return "You win! Paper beats Rock";
+    else if (computerSelection == "Paper" && playerSelection == "Scissor")
+        return "You win! Scissors beat paper";
+    else if (computerSelection == "Scissor" && playerSelection == "Rock")
+        return "You win! Rock beats Scissors";
+    else if (computerSelection == "Scissor" && playerSelection == "Paper")
+        return "You lose! Scissors beat Paper";
+    else if (computerSelection == "Paper" && playerSelection == "Rock")
+        return "You lose! Paper beats Rock";
+    else if (computerSelection == "Rock" && playerSelection == "Scissor")
+        return "You Lose! Rock beats Scissors";
 }
 
-// /*Makes the player's selection case insensitive*/
-// function capitalizeFirstLetter(string) {
-//     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-// }
+/*Makes the player's selection case insensitive*/
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
